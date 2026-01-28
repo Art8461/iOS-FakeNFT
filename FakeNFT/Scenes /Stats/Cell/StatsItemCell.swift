c38f7187fa18b18809265d272cd5e3168f5a070b
art8461 <art8461@gmail.com>
2026-01-28 22:46:58 +0200

    fix: показ плейсхолдер аватара

--- FakeNFT/Scenes /Stats/Cell/StatsItemCell.swift
+++ FakeNFT/Scenes /Stats/Cell/StatsItemCell.swift
@@ -28,7 +28,11 @@ final class StatsItemCell: UICollectionViewListCell, ReuseIdentifying {
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
     func configure(with model: StatsItemCellModel) {
-        previewAvatarImgView.kf.setImage(with: model.avatar)
+        previewAvatarImgView.tintColor = .greyUniversal
+        previewAvatarImgView.kf.setImage(
+            with: model.avatar,
+            placeholder: UIImage(resource: .profile)
+        )
         nameLabel.text = String(model.name)
         countNftLabel.text = String(model.nfts.count)
         countRaitLanel.text = String(model.rating)
